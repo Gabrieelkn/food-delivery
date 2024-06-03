f3c8e85443869fa261e0696622d448552522cb8e
Gabrieelkn <112697117+Gabrieelkn@users.noreply.github.com>
2024-06-03 21:57:07 +0300

    add logo

--- components/header.js
+++ components/header.js
@@ -1,11 +1,16 @@
-import "../styles/index.css";
 import NavigationBar from "./navbar";
 import SearchBar from "./searchBar";
+import Logo from "../public/logo.png";
+import Link from "next/link";
+import Image from "next/image";
 
 export default function Header() {
   return (
     <header className="header">
       <div className="header-container">
+        <Link href="/">
+          <Image src={Logo} width={80} height={80} alt="logo" />
+        </Link>
         <SearchBar />
         <NavigationBar />
       </div>
