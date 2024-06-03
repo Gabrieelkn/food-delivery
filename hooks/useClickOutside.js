889e9af5352b2a59364156deda701081ceb69082
Gabrieelkn <112697117+Gabrieelkn@users.noreply.github.com>
2024-06-03 21:55:18 +0300

    create new custom hook

---
+++ hooks/useClickOutside.js
@@ -0,0 +1,19 @@
+import { useEffect } from "react";
+
+const useOutsideClick = (ref, handler) => {
+  useEffect(() => {
+    const handleOutsideClick = (event) => {
+      if (ref.current && !ref.current.contains(event.target)) {
+        handler(event);
+      }
+    };
+
+    document.addEventListener("mousedown", handleOutsideClick);
+
+    return () => {
+      document.removeEventListener("mousedown", handleOutsideClick);
+    };
+  }, [ref, handler]);
+};
+
+export default useOutsideClick;
